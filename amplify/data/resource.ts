ec5e6ce7a27a12caf55ea95da521caaff0e11661
nik-kita <nik.deno.dev@gmail.com>
2024-10-04 19:58:57 +0200

    deno fmt

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { a, type ClientSchema, defineData } from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -32,7 +32,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+</script>
 
 <template>
-  <details>
-    <summary>hello</summary>
-    <h1>world!</h1>
-  </details>
+  <fieldset>
+    <legend>Free Content</legend>
+    <details>
+      <summary>hello</summary>
+      <h1>world!</h1>
+    </details>
+  </fieldset>
+  <hr />
+  <fieldset>
+    <legend>Private Content</legend>
+    <details>
+      <summary>I'm ready</summary>
+      <authenticator>
+        <template v-slot="{ signOut }">
+          <button @click="signOut">Sign Out</button>
+        </template>
+      </authenticator>
+    </details>
+  </fieldset>
 </template>
